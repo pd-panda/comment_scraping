8d8b9f5f3dd13b0fc679fa3f6118fe7a84384f26
tahaihahimazin <tagainao@icloud.com>
2020-08-18 11:20:20 +0900

    完全マルウェア化
    足りないモジュールを自動インストール

--- __init__.py
+++ __init__.py
@@ -1,5 +1,46 @@
 #-*- coding: utf-8 -*-
+import os
+
+try:
+  import kivy
+except ImportError:
+  print("Trying to Install required module: kivy")
+  os.system('conda install -c conda-forge kivy')
+
+try:
+  import numpy
+except ImportError:
+  print("Trying to Install required module: numpy")
+  os.system('conda install -c anaconda numpy')
+
+try:
+  import matplotlib
+except ImportError:
+  print("Trying to Install required module: matplotlib")
+  os.system('conda install -c conda-forge kivy-garden')
+  os.system('conda install matplotlib==2.2.2')
+  os.system('garden install matplotlib')
+
+try:
+  import pandas
+except ImportError:
+  print("Trying to Install required module: pandas")
+  os.system('conda install -c anaconda pandas')
+
+try:
+  import bs4
+except ImportError:
+  print("Trying to Install required module: bs4")
+  os.system('conda install -c conda-forge bs4')
+
+try:
+  import selenium
+except ImportError:
+  print("Trying to Install required module: selenium")
+  os.system('conda install -c conda-forge selenium')
+
 from Source.showwidget import TestApp
+
 
 if __name__ == '__main__':
     shw = TestApp()
