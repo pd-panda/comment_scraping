378f2b63ea6e1636c184fb3fcaf4100e8c59560c
tahaihahimazin <tagainao@icloud.com>
2020-09-24 19:44:32 +0900

    リファクタリング・ファイル整理

--- Source/showwidget.py
+++ Source/showwidget.py
@@ -30,9 +30,6 @@ import sys, os
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from PIL import Image
 
-# Clockで描画を更新
-from kivy.clock import Clock
-
 import pandas as pd
 
 #from Source.commentscraper import Scraper
@@ -45,7 +42,6 @@ from kivy.lang import Builder
 
 Builder.load_file("./Source/test.kv")
 
-#scr = Scraper()
 dataglp = DataGraph()
 getcomment = GetComment()
 scr = Scraper()
@@ -67,53 +63,26 @@ class SubGrids(BoxLayout):
     """init時にラベル名を引数として与える"""
     def __init__(self, word, **kwargs):
         super().__init__(**kwargs)
-        #self.height= self.minimum_height
-        #self.clear_widgets()
-        #GridLayout.__init__(self, cols=2, raws=1)
-
-        #self.height = self.parent.height * 0.05
-        #self.size_hint_y = None
         self.add_widget(CheckBox(size_hint_x=None, width=75))
-        self.add_widget(Label(text=word, color=[0.23,0.23,0.23,1]))
-        
+        self.add_widget(Label(text=word, color=[0.23,0.23,0.23,1]))      
 
-class ConfigPanel(BoxLayout):
+
+class ConfigPanel(GridLayout):
     def __init__(self, word, **kwargs): 
         super().__init__(**kwargs)
-        #GridLayout.__init__(self, cols=1, rows=3)
-        # test用　データがなくても表示されるよ
-        #GridLayout.__init__(self, cols=1, rows=3)
-        self.clear_widgets()
-        self.orientation = "vertical"
-        #self.height= self.minimum_height
-        self.add_widget(Label(text=word, color=[0.23,0.23,0.23,1]))
-        self.add_widget(Label(text=word, color=[0.23,0.23,0.23,1]))
-        self.add_widget(Label(text=word, color=[0.23,0.23,0.23,1]))
-        
-    # Widget追加時にword抽出をする
-    #def make_words_list(self, word):
-        """
         try:
             word_df = dataglp.get_all_word()
         except:
             print("ファイルが読み込まれていません")
         else:
             print("ファイルが読み込まれました")
-            #self.add_widget(Label(text=word, color=[0.23,0.23,0.23,1]))
-            #self.add_widget(SubGrids(word="aaa"))
             self.clear_widgets()
-            print(len(word_df))
             GridLayout.__init__(self, cols=1, rows=len(word_df))
-            #self.height = self.height * 0.05
-            #self.size_hint_y = None
-            #self.add_widget(Label(text="単語一覧", color=[0.23,0.23,0.23,1]))
-            #self.add_widget(Label(text=word, color=[0.23,0.23,0.23,1]))
-            #GridLayout.__init__(self, cols=2, rows=-(-len(word_df)//2))
             for _, str_word in (dataglp.get_all_word()).iteritems():
                 print(str_word)
                 self.add_widget(SubGrids(word=str_word))
-        """
         pass
+
 
 
 class Panels(GridLayout):
@@ -121,95 +90,26 @@ class Panels(GridLayout):
     def __init__(self, **kwargs): 
         super().__init__(**kwargs)
         lists = ["tagai","hamada"]
-        self.orientation = "vertical"
-        GridLayout.__init__(self, cols=1, rows=3)
-        #GridLayout.__init__(self, cols=1, rows=len(lists))
-        # 縦に分割してラベルを表示できるようにはなった
-        #self.add_widget(Label(text= lists[0]))
-        #self.add_widget(Label(text= lists[0]))
-        #self.add_widget(Label(text= lists[1]))
-        
-        # 縦に（チェックボックス , ラベル）の塊を表示したい
-        self.add_widget(ConfigPanel("aaa"))
-        self.add_widget(ConfigPanel("bbb"))
-        self.add_widget(ConfigPanel("ccc"))
-        
-        #self.add_widget(ConfigPanel("ddd"))
-        
     def make_config_panel(self, lists):
-        #self.clear_widgets()
-        print(len(lists))
-        """
-        self.height= self.minimum_height
+        # 呼び出される度にWidgetを初期化する
+        self.clear_widgets()
+        # リストの数だけパネルのGridLayoutを準備する
         GridLayout.__init__(self, cols=1, rows=len(lists))
-
-        #GridLayout.__init__(self, cols=1, rows=len(lists))
-        #self.add_widget(ConfigPanel("aaa"))
-        #self.add_widget(ConfigPanel("bbb"))
+        # リストの数だけConfigPanelをWidgetに追加
         for text in lists:
             print(text)
             self.add_widget(ConfigPanel(text))
-        """
 
-        """
-        , 
-                            orientation = 'vertical',
-                            size_hint_y = None,
-                            pos_hint    = {"x":0, "top":1},
-                            height      = self.parent.height,
-                            size_hint_x = 0.5,
-                            padding     = [10, 30],
-                            spacing     = [20, 50],
-        """
-        #with self.canvas:
-            #Color(0, 1, 1, 1)
-        #    self.rect = Rectangle(source="./Source/image/commentpanel.png", size=self.size, pos=self.pos)
-            #self.rect = Rectangle(source="back.jpg", size=root.size, pos=root.pos)
-        #self.add_widget()
-        """
-        canvas.before: 
-            Rectangle: 
-                pos: self.pos 
-                size: self.size 
-                source: './Source/image/commentpanel.png'
-                Label:
-        text: "コメント数"
-            color: [0.23,0.23,0.23,1]
-            font_size: 32
-            halign: 'center'
-            valign: 'middle'
-            height: 30
-            size_hint_y: None
-            #size_hint_y: 0.1
-        Image:
-            #size: self.size
-            height: 20
-            size_hint_y: None
-            #size_hint_y: 0.1
-            keep_ratio: True
-            source: './Source/image/line.png'
-        """
-        #confpanel = self.ids.conpanel
-        #confpanel = ConfigPanel()
-        #self.add_widget(confpanel.make_words_list("aaa"))
-        #for text in lists:
-        #    print(text)
-        #    self.add_widget(confpanel.make_words_list(text))
-
-#check = BooleanProperty(True)
 class GraphView(BoxLayout):
     """Matplotlib のグラフを表示するためのウィジェット"""
     check = BooleanProperty(True)
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.state = True
         # 描画領域を用意する
-        #self.fig, self.ax = plt.subplots()
         self.fig = plt.figure(figsize=(16, 9))
         self.ax = self.fig.add_subplot(111)
         # 描画を初期化する
         self.init_view()
-
         # グラフをウィジェットとして追加する
         widget = FigureCanvasKivyAgg(self.fig)
         self.add_widget(widget)
@@ -217,26 +117,6 @@ class GraphView(BoxLayout):
     def init_view(self):
         # 以前の内容を消去する
         self.ax.clear()
-        #self.state = True
-        #画像の読み込み
-        #im = Image.open(os.path.join(imagedir, "QRcode.png"))
-
-        #画像をarrayに変換
-        #im_list = np.array(im)
-        #self.ax.plot(x, y, label="test")
-        #self.ax.set_title("test", color='red')
-        # データを描画する
-        #self.ax.imshow(im_list, im_list.shape[0], im_list.shape[1], im_list.shape[2])
-        #img = plt.imread(os.path.join(imagedir, "QRcode.png"))
-        #self.plt.imshow(img) # 画像の描画
-        #self.get_center_yplt.show() # 描画結果の表示
-
-        self.ax.axis('off')
-
-        # 再描画する
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
-
 
         # 再描画する
         self.fig.canvas.draw()
@@ -259,6 +139,7 @@ class GraphView(BoxLayout):
             return
         print(df)
         dataglp.init_graph(df)
+        # 最初のグラフを出力
         self.update_view(df, "func1")
     
     def load_scrapingdata(self):
@@ -292,38 +173,43 @@ class GraphView(BoxLayout):
         return
     
     def plot_graph(self, funcname):
-        print(funcname)
+        """dataglp.switch_graph(fig, ax, グラフタイトル, 呼び出し関数名)"""
         if funcname == "func1":
-            print("func1 play")
+            # 単語出現数の推移の表示
+            print("単語出現数の推移 plot")
             dataglp.switch_graph(self.fig, self.ax, "単語出現数の推移", "df_time_word_point_line_100")
-            #configpanel = self.ids.conpanel
-            #print(configpanel)
-            #configpanel = ConfigPanel()
-            #configpanel.make_words_list()
-            #ConfigPanel.make_words_list()
         elif funcname == "func2":
-            print("func2 play")
+            # ホットワードの表示
+            print("ホットワード plot")
             dataglp.switch_graph(self.fig, self.ax, "ホットワード", "treemap")
         elif funcname == "func3":
-            print("func3 play")
+            # コメントの多い投稿者の表示
+            print("コメントの多い投稿者 play")
             dataglp.switch_graph(self.fig, self.ax, "コメントの多い投稿者", "bargraph_contributor")
         elif funcname == "func4":
-            print("func4 play")
+            # 笑いの推移の表示
+            print("笑いの推移 play")
             dataglp.switch_graph(self.fig, self.ax, "笑いの推移", "df_time_www_point_100")
         elif funcname == "func5":
-            print("func5 play")
+            # 拍手の推移の表示
+            print("拍手の推移 play")
             dataglp.switch_graph(self.fig, self.ax, "拍手の推移", "df_time_hakusyu_point_100")
         elif funcname == "func6":
+            # 参照URLランキングの表示
             print("func6 play")
             dataglp.switch_graph(self.fig, self.ax, "参照URLランキング", "urltable")
         elif funcname == "func7":
+            # 参照URLランキングの表示
+            print("func7 play")
             dataglp.switch_graph(self.fig, self.ax, "参照URLランキング", "urltable")
 
 
 
 class ShowWidget(Widget):
-    
-    text = StringProperty()    # プロパティの追加
+    """Widget全体を管理するスクリプト"""
+
+    #------ 共通プロパティの宣言 ---------------
+    text = StringProperty()
     source  = StringProperty(imagedir + 'lefton.png')
     color = ListProperty([1,1,1,1])
     fileinputimage = StringProperty(imagedir + 'nomalimage.png')
@@ -331,78 +217,60 @@ class ShowWidget(Widget):
     check1 = BooleanProperty(False)
     check2 = BooleanProperty(False)
     check3 = BooleanProperty(False)
-    #df = pd.DataFrame()
+
+    #------ ShowWidgetの初期化 ---------------
     def __init__(self, **kwargs):
         super(ShowWidget, self).__init__(**kwargs)
         Window.bind(on_dropfile=self._on_file_drop)
-        self.text = 'How are you?'
-        print("pressed")
-        self.color = [1, 1, 1 , 1]
 
-    def buttonClicked1(self):        # ボタンをクリック時
-        self.text = 'I am fine!'
-        print("pressed")
-        #df = getcomment.main(self.filepath, savefilepath)
-        print("buttonclickedfunc")
-        print(df)
-        #glp.update_view(df)
-        self.color = [1, 0, 1 , 1]
-    
-    def buttonClicked2(self):        # ボタンをクリック時
-        self.text = 'I am soso!'
-        print("pressed")
-        self.color = [1, 1, 0 , 1]
-        #glp.test_sin_show()
-    
-    def buttonClicked3(self):        # ボタンをクリック時
-        self.text = 'I am happy!'
-        print("pressed")
-        self.color = [0, 1, 1 , 1]
-
-    def buttonClickedroom(self):        # ボタンをクリック時
-        #self.text = 'room名より取得'
+    #------ ボタンイベント --------------------
+    def buttonClickedroom(self):
+        # 左のタブがクリックされた時
+        # 表示画像の変更
         self.source = imagedir + 'lefton.png'
-        self.color = [0, 1, 1 , 1]
         print("buttonClickedroom!!!")
 
-    def buttonClickedfile(self):        # ボタンをクリック時
-        #self.text = 'ファイルから取得'
+    def buttonClickedfile(self):
+        # 右のタブがクリックされた時
+        # 表示画像の変更
         self.source = imagedir + 'righton.png'
-        self.color = [0, 1, 1 , 1]
         print("buttonClickedfile!!!")
 
     def checkbox_check1(self, checkbox):
+        # チェックボックス1が押された時
         self.check1 = checkbox.active
         return
     
     def checkbox_check2(self, checkbox):
+        # チェックボックス2が押された時
         self.check2 = checkbox.active
         return
     
     def checkbox_check3(self, checkbox):
+        # チェックボックス3が押された時
         self.check3 = checkbox.active
         return
 
-    def plottest(self):
-        glp.main()
-
+    #------ ウィンドウイベント -----------------
+    def _on_file_drop(self, window, file_path):
+        # ウィンドウにドロップ＆ドラッグされたファイルの名前を取得
+        global loadfilepath
+        # ファイル名を格納
+        loadfilepath = file_path.decode()
+        # 表示ファイル名を変更
+        self.filepath = os.path.basename(file_path.decode())
+    
+    #------ 設定パネルの再描画 -----------------
     def conpanel(self):
-        print("conpanel press")
+        # 選択されたグラフの種類で設定パネルの中身を変更する
         graphpanel = self.ids.graph_view
         graphpanel.load_data()
 
         configpanel = self.ids.conpanel
         configpanel.make_config_panel(["tagai","hamada"])
 
-    # Windowにドロップされた際発生するイベント
-    # ファイル名を取得する
-    def _on_file_drop(self, window, file_path):
-        global loadfilepath
-        loadfilepath = file_path.decode()
-        self.filepath = os.path.basename(file_path.decode())
-        self.fileinputimage = imagedir + 'inputimage.png'
-
 class CommentShowApp(App):
+    """アプリの管理をするスクリプト"""
 
     def __init__(self, **kwargs):
         super(CommentShowApp, self).__init__(**kwargs)
